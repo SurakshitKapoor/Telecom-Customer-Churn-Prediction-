3a46cce3e2f0b456b39ddb5987a495038535fda9
Surakshit <surakshitkapoor14@gmail.com>
2026-05-02 23:08:59 +0530

    add config and core changes

--- churn_fastapi_ml/app/core/logger.py
+++ churn_fastapi_ml/app/core/logger.py
@@ -1,23 +1,15 @@
 
+
 import logging
 import os
-from datetime import datetime
+from app.core.config import settings
 
 os.makedirs("logs", exist_ok=True)
 
-# file name as per current date and time -> to know when the model is created and all!
-log_file = f"logs/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-
 logging.basicConfig(
-    filename=log_file,
-    level=logging.INFO,
+    filename="logs/app.log",
+    level=settings.log_level,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
 logger = logging.getLogger(__name__)
-
-if __name__ == "__main__":
-    print("We are in logger.py")
-    logger.info("Logger is working!")
-    print(f"Logging to: {log_file}")
-    print(os.getcwd())
--- churn_fastapi_ml/app/model_loader.py
+++ churn_fastapi_ml/app/model_loader.py
@@ -1,9 +1,15 @@
 
 
+# import joblib
+# import os
+
+# def load_model():
+#     base_dir = os.path.dirname(os.path.dirname(__file__))  # project root
+#     model_path = os.path.join(base_dir, "models", "churn_model.pkl")
+#     return joblib.load(model_path)
+
 import joblib
-import os
+from app.core.config import settings
 
 def load_model():
-    base_dir = os.path.dirname(os.path.dirname(__file__))  # project root
-    model_path = os.path.join(base_dir, "models", "churn_model.pkl")
-    return joblib.load(model_path)
+    return joblib.load(settings.model_path)
